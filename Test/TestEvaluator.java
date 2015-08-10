105f3c8cea5c6ce60e33c413e658667b00935f71
heyayes <stshakun@gmail.com>
2015-08-10 10:34:17 +0300

    Added Test code

--- Test/TestEvaluator.java
+++ Test/TestEvaluator.java
@@ -2,16 +2,24 @@
  * Created by stshakun on 09.08.15.
  */
 
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.*;
+import org.junit.Assert.*;
 import net.sourceforge.jeval.Evaluator;
 import net.sourceforge.jeval.EvaluationException;
 
+public class TestEvaluator extends org.junit.Assert {
+    static Evaluator eva = new Evaluator();
 
-public class TestEvaluator {
+    @Test
+    public void testEvaluator() {
 
-    public static void main(String[] args) {
-        int a = 1 + 1 + 1;
+        try {
+            String result = (eva.evaluate("4+3"));
+            assertTrue("Resul is " + result + " not equals", result.equals("7.0"));
+        } catch (EvaluationException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
